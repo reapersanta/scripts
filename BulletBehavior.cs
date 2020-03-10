58c9ea72f7bfe35e6ae0454a269e97c92772ddba
Miles Salumbre <39288585+reapersanta@users.noreply.github.com>
2020-03-10 19:16:21 +0800

    Create BulletBehavior.cs

---
+++ BulletBehavior.cs
@@ -0,0 +1,90 @@
+using System;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+// feel free to add some type of Bullet
+	public enum TypeOfBullet
+	{
+		Constant,
+		Punchy,
+		Rocket,
+	}
+
+	  public TypeOfBullet BulletType;
+    public bool MoveWithTransform = true;
+    public float TimeToDestroy = 8f;
+    public float Speed;
+    [Header("If Punchy or Rocket")] 
+    public bool UseAsBoomerang;
+    public float MoveIncrement;
+    [Range(1f, 5f)] public float Punchyness;
+    [Range(1f, 50f)] public float Damping;
+    [Header("If not moving with transform")]
+    public Vector2 FreeMovement;
+    public GameObject Explosion;
+    public int BulletDamage;
+
+    private Transform _myTransform;
+    
+    private void Start()
+    {
+	    _myTransform = this.transform;
+        Invoke(nameof(Kill),TimeToDestroy);
+    }
+
+    void Update()
+    {
+        if(MoveWithTransform) // when moveWithTransform is True it will move with rotation according to its transform.Up
+        {
+	        switch (BulletType)
+	        {
+		        case TypeOfBullet.Constant:
+			        _myTransform.position += Time.deltaTime * Speed * _myTransform.up; 
+			        break;
+		        case TypeOfBullet.Punchy:
+			        _myTransform.position += Time.deltaTime * MoveIncrement * Punchyness *
+			                                 _myTransform.up;
+			        if (MoveIncrement <= 0f && !UseAsBoomerang)
+			        {
+				        MoveIncrement = 0f;
+				        return;
+			        }
+			        MoveIncrement -= Time.deltaTime * Damping;
+			        break;
+		        case TypeOfBullet.Rocket:
+			        _myTransform.position += Time.deltaTime * MoveIncrement * _myTransform.up;
+			        if (MoveIncrement <= 0f && !UseAsBoomerang)
+			        {
+				        MoveIncrement = 0f;
+				        return;
+			        }
+			        MoveIncrement += Time.deltaTime * Punchyness;
+			        break;
+		        default:
+			        throw new ArgumentOutOfRangeException();
+	        }
+        } else // when moveWithTransform is false it will move through its axis
+        {
+            _myTransform.position += (Vector3)FreeMovement*Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+		{
+			other.gameObject.GetComponent<ITakeDamage>()?.TakeDamage(BulletDamage);
+		    Kill();
+        } else if (other.gameObject.CompareTag("Block"))
+        {
+            Kill();
+        }
+	}
+
+	private void Kill()
+	{
+		Instantiate(Explosion, _myTransform.position, Quaternion.identity);
+		Destroy(this.gameObject);
+	}
+}
