6485aff917bf18cd27b766ee49093ffb0f18e154
Miles Salumbre <39288585+reapersanta@users.noreply.github.com>
2020-03-10 19:18:31 +0800

    Update BulletBehavior.cs

--- BulletBehavior.cs
+++ BulletBehavior.cs
@@ -10,22 +10,22 @@ public class BulletBehavior : MonoBehaviour
 		Punchy,
 		Rocket,
 	}
+	// sorry i don't know how to name variables right, feel free to change or modify
+	public TypeOfBullet BulletType;
+	public bool MoveWithTransform = true;
+	public float TimeToDestroy = 8f;
+	public float Speed;
+	[Header("If Punchy or Rocket")] 
+	public bool UseAsBoomerang;
+	public float MoveIncrement;
+	[Range(1f, 5f)] public float Punchyness;
+	[Range(1f, 50f)] public float Damping;
+	[Header("If not moving with transform")]
+	public Vector2 FreeMovement;
+	public GameObject Explosion;
+	public int BulletDamage;
 
-	  public TypeOfBullet BulletType;
-    public bool MoveWithTransform = true;
-    public float TimeToDestroy = 8f;
-    public float Speed;
-    [Header("If Punchy or Rocket")] 
-    public bool UseAsBoomerang;
-    public float MoveIncrement;
-    [Range(1f, 5f)] public float Punchyness;
-    [Range(1f, 50f)] public float Damping;
-    [Header("If not moving with transform")]
-    public Vector2 FreeMovement;
-    public GameObject Explosion;
-    public int BulletDamage;
-
-    private Transform _myTransform;
+	private Transform _myTransform;
     
     private void Start()
     {
@@ -70,7 +70,7 @@ public class BulletBehavior : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.gameObject.CompareTag("Player"))
 		{
